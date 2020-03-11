f9dd9a20b97226b2cdc511a6c96d77c503ab976d
Mackenzie Davidson <mwvdavidson@gmail.com>
2020-03-11 11:24:20 -0400

    google analytics

--- js/ours/g.js
+++ js/ours/g.js
@@ -2,7 +2,7 @@ function loadGoogleAnalytics() {
   const ga = document.createElement('script')
   ga.type = 'text/javascript'
   ga.async = true
-  ga.src = 'https://www.googletagmanager.com/gtag/js?id=YOUR-TRACKING-CODE'
+  ga.src = 'https://www.googletagmanager.com/gtag/js?id=UA-160325316-1'
 
   const s = document.getElementsByTagName('script')[0]
   s.parentNode.insertBefore(ga, s)
@@ -16,4 +16,4 @@ function gtag() {
 }
 gtag('js', new Date())
 
-gtag('config', 'YOUR-GA-TRACKING-CODE')
+gtag('config', 'UA-160325316-1')
