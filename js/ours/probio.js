9d791dd79c1b0553d778ee53d866e7a3fd05e5c8
Mackenzie Davidson <mwvdavidson@gmail.com>
2020-03-12 14:01:17 -0400

    removed unused code comments

--- js/ours/probio.js
+++ js/ours/probio.js
@@ -13,17 +13,4 @@ $(document).ready(function() {
       $('#hc').removeClass('hc-active')
     }
   })
-  // cleaner but there is a split second of no stylesheet at all lol
-  // $('#hc').click(function() {
-  //   const hcA = $('#hc').hasClass('hc-active')
-  //   const egg = '/stylesheets/css/egg.css'
-  //   const hc = '/stylesheets/css/hc.css'
-  //   if (hcA !== true) {
-  //     $(`link[href="${egg}"]`).attr('href', `${hc}`)
-  //     $('#hc').addClass('hc-active')
-  //   } else {
-  //     $(`link[href="${hc}"]`).attr('href', `${egg}`)
-  //     $('#hc').removeClass('hc-active')
-  //   }
-  // })
 })
