dbb01dd322c393526131b22a235d0185d9454a68
Mackenzie Davidson <mwvdavidson@gmail.com>
2020-03-12 14:09:02 -0400

    green microbes bb

--- js/ours/probio.js
+++ js/ours/probio.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
     const hc = '/stylesheets/css/hc.css'
     if (hcA !== true) {
       $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', '/stylesheets/css/hc.css'))
+      // $('#sh-hc').show()
+      // $('#sh-base').hide()
       $('#hc').addClass('hc-active')
     } else {
       $(`link[href="${hc}"]`).remove()
